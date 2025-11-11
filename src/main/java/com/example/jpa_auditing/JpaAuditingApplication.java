fe870dacad535e767db158de1d7f4ad69a717fe7
vinimanfrin <viniciusmanfrin2019@gmail.com>
2025-11-11 15:37:51 -0300

    FEAT: criação do projeto

---
+++ src/main/java/com/example/jpa_auditing/JpaAuditingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_auditing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAuditingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAuditingApplication.class, args);
+	}
+
+}
